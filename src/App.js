9fceca632fd9961fbba6fc38cd3d7a7b84c5d56c
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-10-06 16:20:03 +0300

    created react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import hero from "./hero-ai.png";
+import Footer from "./components/Footer";
+import HomeMain from "./components/HomeMain";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Navbar logo={logo} />
+        <Hero hero={hero} />
       </header>
+      <HomeMain />
+      <Footer />
     </div>
   );
 }
